366b4f9ce615c108d4576a8da09fc38f75bf07c4
Yulia <yulia.tatarkina3@gmail.com>
2025-12-14 15:16:04 +0300

    Fix checkstyle

--- src/main/java/ru/practicum/shareit/constants/HeaderConstants.java
+++ src/main/java/ru/practicum/shareit/constants/HeaderConstants.java
@@ -3,5 +3,6 @@ package ru.practicum.shareit.constants;
 public class HeaderConstants {
     public static final String USER_ID = "X-Sharer-User-Id";
 
-    private HeaderConstants() {}
+    private HeaderConstants() {
+    }
 }
